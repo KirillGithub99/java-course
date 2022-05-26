1846102d55ba89165613c33b241aa2858375bea0
KirillGithub99 <kt.karpenko@mail.ru>
2022-05-26 23:23:20 +0300

    Test commit 3

--- src/com/java/by/it_academy/lesson2/main.java
+++
@@ -1,5 +0,0 @@
-package com.java.by.it_academy.lesson2;
-
-public class main {
-    }
-
---
+++ src/java/com/by/it_academy/lesson2/Main.java
@@ -0,0 +1,16 @@
+package com.java.by.it_academy.lesson2;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        String name = "Kirill";
+        int age = 22;
+        float height = 1.7f;//написал флоат типо
+    }
+
+
+}
+
+
