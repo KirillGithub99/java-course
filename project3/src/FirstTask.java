5d8787476ee5c6ac831a50e6e9cc1314533683ad
KirillGithub99 <105937942+KirillGithub99@users.noreply.github.com>
2022-05-31 00:26:00 +0300

    Add files via upload

---
+++ project3/src/FirstTask.java
@@ -0,0 +1,25 @@
+public class FirstTask {
+    public static void main(String[] args) {
+
+
+        int giftValue = 6000;
+
+        int minPriceTax = 0;
+        double percentOfTax = 0.0;
+        if (giftValue >= 5000 && giftValue <= 25000) {
+            minPriceTax = 100;
+            percentOfTax = 0.08;
+        } else if (giftValue > 25000) {
+
+            minPriceTax = 1700;
+            percentOfTax = 0.1;
+        }
+       double result = minPriceTax + (giftValue - 5000) * percentOfTax;
+
+        System.out.println(result);
+
+    }
+
+}
+
+
---
+++ project3/src/ThirdTask.java
@@ -0,0 +1,13 @@
+public class ThirdTask {
+    public static void main(String[] args) {
+        String password = "caput draconis";
+        if (password == "caput draconis") {
+            System.out.println("Welcome");
+        } else  {
+            System.out.println("Forbidden");
+
+        }
+
+
+    }
+}
