081f4f8e1e097dcb47adefa744a94f3862a5d058
KirillGithub99 <kt.karpenko@mail.ru>
2022-08-04 19:34:51 +0300

    commit

---
+++ src/main/java/by/it_academy/lesson7/TextReverse.java
@@ -0,0 +1,23 @@
+package by.it_academy.lesson7;
+
+public class TextReverse {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("FOREVER");
+        stringBuilder.reverse();
+        System.out.println(stringBuilder);
+
+        /*
+        String text = "FOREVER";
+        String[] arrayText = text.split("");
+        int j = 0;
+        while (j < arrayText.length-1){
+        j++;
+        }
+        for (int i = j; i > 0; i--)
+        {
+            System.out.println(arrayText[i]);
+        }
+        */
+    }
+
+}
