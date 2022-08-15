9364ad427edc0dc05d69a28926f3e8001997f8f0
KirillGithub99 <kt.karpenko@mail.ru>
2022-08-15 18:18:11 +0300

    commit

---
+++ src/main/java/by/it_academy/lesson11/AverageSensor.java
@@ -0,0 +1,65 @@
+package by.it_academy.lesson11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AverageSensor implements Sensor {
+    private List<Sensor> arrayOfSensor = new ArrayList<>();
+
+    void addSensor(Sensor toAdd) {
+        arrayOfSensor.add(toAdd);
+    }
+
+
+    @Override
+    public boolean IsOn() {
+        boolean sensorStatus = false;
+        for (Sensor sensor : arrayOfSensor) {
+            if (sensor.IsOn() == false) ;
+            sensorStatus = false;
+        }
+        else
+        {
+            sensorStatus = true;
+        }
+        return sensorStatus;
+    }
+
+
+    @Override
+    public void setOn() {
+        for (Sensor sensor : arrayOfSensor) {
+            sensor.setOn();
+        }
+    }
+
+    @Override
+    public void setOff() {
+        for (Sensor sensor : arrayOfSensor) {
+            sensor.setOff();
+        }
+    }
+
+    @Override
+    public int read() {
+        int sum = 0;
+        int count = 0;
+        for (Sensor sensor : arrayOfSensor) ;
+        if (Sensor.IsOn() == true) {
+            sum = Sensor.read();
+            count++;
+        }
+    } if(count == 0){
+
+        IllegalStateException();
+    }
+    return sum /count;
+}
+
+
+
+
+
+
+
+
